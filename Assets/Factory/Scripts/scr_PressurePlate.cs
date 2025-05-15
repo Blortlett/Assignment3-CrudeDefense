75b5a9a6d517eee46df2dd8ade956b9f4fc7b320
Blortlett <45956871+Blortlett@users.noreply.github.com>
2025-05-16 10:36:50 +1200

    Added PressurePlate & button to press

---
+++ Assets/Factory/Scripts/scr_PressurePlate.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PressurePlateScript : MonoBehaviour
+{
+    [SerializeField] Transform mPressurePadGraphic;
+
+    Collider2D mPressurePlateTrigger;
+    bool mIsPressed;
+    Vector2 mButtonUnpressedPosition = new Vector2(0, 0.2539f);
+    Vector2 mButtonPressedPosition = new Vector2(0, 0);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mPressurePlateTrigger = gameObject.GetComponent<Collider2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (mIsPressed)
+        {
+            mPressurePadGraphic.localPosition = mButtonPressedPosition;
+        }
+        else // not pressed
+        {
+            mPressurePadGraphic.localPosition = mButtonUnpressedPosition;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        mIsPressed = true;
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        mIsPressed = false;
+    }
+}
