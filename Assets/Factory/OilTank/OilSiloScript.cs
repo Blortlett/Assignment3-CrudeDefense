5f2144fb0b55b7dbc8324371381e0928db74a441
Blortlett <45956871+Blortlett@users.noreply.github.com>
2025-05-23 18:06:32 +1200

    Fixed up the oil silo a bit more

--- Assets/Factory/OilTank/OilSiloScript.cs
+++ Assets/Factory/OilTank/OilSiloScript.cs
@@ -5,35 +5,33 @@ using UnityEngine.UI;
 
 public class OilSiloScript : MonoBehaviour, IButtonable
 {
-    // Slider varaibles (OilLevel Calculations)
-    [SerializeField] private Slider mOilLevelSlider; // Reference to the Slider component   // This was a stupid idea, unhook this and delete the slider component for all I care
-    [SerializeField] private Sprite mOilLevelSprite; // Reference to the oil level sprite
+    // Oil level visuals
+    [SerializeField] private Transform mOilLevelParent; // Reference to the oil level sprite
     private const float mMaxOilLevel = 100f;
-    private float mCurrentOilLevel;
-    private const float mMinimumOilLevel = 30f;
+    private float mCurrentOilLevel = 0f;
     // Rate at which the tank fills
-    private const float mFillRate = 20f;
-
+    [SerializeField] private const float mFillRate = 20f;
     // Silo running/filling variables
     public bool mMachineRunning = false;
+    // MaxGraphicSize
+    [SerializeField] private float mMaxOilGraphicYScale = 3;
 
     // Start is called before the first frame update
     void Start()
     {
-        mCurrentOilLevel = 30f;
+        mCurrentOilLevel = 0f;
     }
 
     // Update is called once per frame
     void Update()
     {
         TryFillOil();
-        mOilLevelSlider.value = mCurrentOilLevel / mMaxOilLevel;
+        UpdateOilLevelVisual();
     }
 
     // This actually runs when wheel turns not on button press but we hacky out here
     public void OnButtonPress()
     { // Toggle machine running on and off
-        Debug.Log("Toggled filling Silo with oil");
         mMachineRunning = !mMachineRunning;
     }
 
@@ -41,17 +39,20 @@ public class OilSiloScript : MonoBehaviour, IButtonable
     private void TryFillOil()
     {
         if (!mMachineRunning) return; // If not filling oil dont worry about this function, just move on
+        if (mCurrentOilLevel > +mMaxOilLevel) return; // If tank already full exit function
+
         Debug.Log("Filling Silo with oil");
-        mCurrentOilLevel += mFillRate * Time.deltaTime;
+        mCurrentOilLevel += mFillRate * Time.deltaTime; // Raise tank oil percentage
     }
 
     private void UpdateOilLevelVisual()
     {
-        if (mOilLevelSprite != null && mOilLevelSlider != null)
+        if (mOilLevelParent != null) // error check
         {
-            //mOilLevelSprite.rect.height = mOilLevelSlider.value;
-            //scale.y = mOilLevelSlider.value;
-            //mOilLevelSprite.localScale = scale;
+            // Set oil graphics size
+            float PercentFull = (mCurrentOilLevel / mMaxOilLevel);
+            float yLevel = 1 + (PercentFull * mMaxOilGraphicYScale);
+            mOilLevelParent.localScale = new Vector2(1, yLevel);
         }
     }
 }
