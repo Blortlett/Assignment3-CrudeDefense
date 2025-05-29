691ecde7b9207c5be6eb8dcd2fc293ede8806b16
OliverNJ <demilavagods@gmail.com>
2025-05-30 09:50:19 +1200

    Added footsteps

--- Assets/Sound/Footsteps.cs
+++ Assets/Sound/Footsteps.cs
@@ -4,6 +4,27 @@ using UnityEngine;
 
 public class Footsteps : MonoBehaviour
 {
-    
+    [SerializeField] private List<AudioSource> FootSteps;
 
+    int LastTriggered = 0;
+
+    public void PlayFootStep()
+    {
+        //LastTriggered =
+        int RandomInt = Random.Range(0, FootSteps.Count - 1);
+        if (RandomInt == LastTriggered)
+        {
+            if (RandomInt != 0)
+            {
+                RandomInt = 0;
+            }
+            else
+            {
+                RandomInt = Random.Range(1, FootSteps.Count - 1);
+            }
+        }
+        FootSteps[RandomInt].pitch = Random.Range(1f, 1.2f);
+        FootSteps[RandomInt].Play();
+        LastTriggered = RandomInt;
+    }
 }
