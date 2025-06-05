0e1cf7094b4397c877e3cde2d97a3082b020ea42
Blortlett <45956871+Blortlett@users.noreply.github.com>
2025-06-05 12:47:26 +1200

    Polar bear sleeps on and pushes pressure plate

--- Assets/Enemies/IndividualAnimalScripts/EnemyPolarBear.cs
+++ Assets/Enemies/IndividualAnimalScripts/EnemyPolarBear.cs
@@ -14,9 +14,19 @@ public class EnemyPolarBear : MonoBehaviour, IEnemies
     private Enemy mEnemyController;
     private Animator mAnimator;
 
+    // timer to ensure polar bear sleeps ontop of button
+    float mMaxTimeToSleep = .5f;
+    float mTimeToSleep;
+    bool mShouldSleep = false;
 
     private void Start()
     {
+        // Set Tag as polarbear
+        gameObject.tag = "PolarBear";
+
+        // Setup timer for sleeping center of button
+        mTimeToSleep = mMaxTimeToSleep;
+        // Get polar bear controller and animator
         mEnemyController = GetComponent<Enemy>();
         mAnimator = mEnemyController.GetComponentInChildren<Animator>();
 
@@ -30,21 +40,24 @@ public class EnemyPolarBear : MonoBehaviour, IEnemies
             Debug.Log("Polar bear has a controller");
         else
             Debug.Log("Polar bear has no controller :(");
-
-        //mEnemyController.mIsTrapped = true; // using this to get old mate to go to take a nap
     }
 
     private void Update()
     {
-
+        if (!mShouldSleep) return;
+        mTimeToSleep -= Time.deltaTime;
+        if (mTimeToSleep <= 0f)
+        {
+            mAnimator.SetBool("IsSleeping", true);
+            mEnemyController.mIsTrapped = true; // using this to get old mate to go to take a nap
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("PressurePlate"))
         {
-            mAnimator.SetBool("IsSleeping", true);
-            mEnemyController.mIsTrapped = true; // using this to get old mate to go to take a nap
+            mShouldSleep = true;
         }
     }
 }
--- Assets/Factory/PressurePlate/PressurePlate.cs
+++ Assets/Factory/PressurePlate/PressurePlate.cs
@@ -8,15 +8,23 @@ public class PressurePlateScript : MonoBehaviour
 
     Collider2D mPressurePlateTrigger;
     bool mIsPressed;
+    bool mIsPressedPolarBear;
     [SerializeField] Transform mButtonUnpressedPosition;
     [SerializeField] Transform mButtonPressedPosition;
 
     [SerializeField] GameObject mPressureplateableObject;
     IPressurePlateable mTargetTriggerableScript;
 
+    // Polar bear pressed timer
+    float mPolarBearPressedTimer;
+    float mPolarBearPressedTimerMax = .65f;
+
     // Start is called before the first frame update
     void Start()
     {
+        // init polar bear timer
+        mPolarBearPressedTimer = mPolarBearPressedTimerMax;
+
         // Get collider for player to jump on to activate pressure plate
         mPressurePlateTrigger = gameObject.GetComponent<Collider2D>();
         // Get script to activate on Pressureplate pushed
@@ -31,7 +39,7 @@ public class PressurePlateScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (mIsPressed)
+        if (mIsPressed || mIsPressedPolarBear)
         {
             // Set graphic to lowered position
             mPressurePadGraphic.position = mButtonPressedPosition.position;
@@ -40,6 +48,20 @@ public class PressurePlateScript : MonoBehaviour
         {
             // Set graphic to raised position
             mPressurePadGraphic.position = mButtonUnpressedPosition.position;
+        }
+
+        // if pressed by polar bear
+        if (mIsPressedPolarBear)
+        {
+            // tick timer down
+            mPolarBearPressedTimer -= Time.deltaTime;
+            //when timer expires
+            if (mPolarBearPressedTimer <= 0f)
+            {
+                mTargetTriggerableScript.PressurePlatePushed();
+
+                mPolarBearPressedTimer = mPolarBearPressedTimerMax;
+            }
         }
     }
 
@@ -51,13 +73,23 @@ public class PressurePlateScript : MonoBehaviour
             // Trigger target script
             mTargetTriggerableScript.PressurePlatePushed();
         }
+
+        if (collision.CompareTag("PolarBear")) // only activate for PolarBear
+        {
+            mIsPressedPolarBear = true;
+        }
     }
 
     private void OnTriggerExit2D(Collider2D collision)
     {
-        if (collision.CompareTag("Player")) // only activate for player
+        if (collision.CompareTag("Player")) // only activate for Player
         {
             mIsPressed = false;
+        }
+
+        if (collision.CompareTag("PolarBear")) // only activate for PolarBear
+        {
+            mIsPressedPolarBear = false;
         }
     }
 }
